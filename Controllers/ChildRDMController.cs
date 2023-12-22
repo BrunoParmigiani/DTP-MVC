3a8c3d5b3dc0c9f4e4fa0162216835c916a81533
Bruno Henrique <brunossj54@gmail.com>
2023-12-22 17:35:40 -0300

    Code structure changes

---
+++ Controllers/ChildRDMController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DTP.Services;
+using DTP.Models.ViewModels;
+using DTP.Services.Exceptions;
+using DTP.Models;
+
+namespace DTP.Controllers
+{
+    public class ChildRDMController : Controller
+    {
+        private readonly DTPsService _dtpsService;
+        private readonly ParentRDMService _parentRDMService;
+        private readonly ChildrenRDMService _childrenRDMService;
+
+        public ChildRDMController(DTPsService dtpsService, ParentRDMService parentRDMService, ChildrenRDMService childrenRDMService)
+        {
+            _dtpsService = dtpsService;
+            _parentRDMService = parentRDMService;
+            _childrenRDMService = childrenRDMService;
+        }
+
+        public IActionResult Create(int? dtpId, int? parentId)
+        {
+            var viewModel = new ChildFormViewModel
+            {
+                DTPId = dtpId.Value,
+                ParentId = parentId.Value
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(ChildrenRDM child, int dtpId, int parentId)
+        {
+            child.Ticket = await _dtpsService.FindByIdAsync(dtpId);
+            child.Parent = await _parentRDMService.FindByIdAsync(parentId);
+            child.OpenDate = DateTime.Now;
+
+            await _childrenRDMService.InsertAsync(child);
+
+            return RedirectToAction("Index", "DTPs");
+        }
+
+        public async Task<IActionResult> Delete(int? dtpId, int? childId)
+        {
+            var child = await _childrenRDMService.FindByIdAsync(childId.Value);
+            child.Ticket = await _dtpsService.FindByIdAsync(dtpId.Value);
+
+            return View(child);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _childrenRDMService.RemoveAsync(id);
+
+            return RedirectToAction("Index", "DTPs");
+        }
+    }
+}
---
+++ Controllers/ParentRDMController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DTP.Services;
+using DTP.Models.ViewModels;
+using DTP.Services.Exceptions;
+using DTP.Models;
+
+namespace DTP.Controllers
+{
+    public class ParentRDMController : Controller
+    {
+        private readonly DTPsService _dtpsService;
+        private readonly ParentRDMService _parentRDMService;
+        private readonly ChildrenRDMService _childrenRDMService;
+
+        public ParentRDMController(DTPsService dtpsService, ParentRDMService parentRDMService, ChildrenRDMService childrenRDMService)
+        {
+            _dtpsService = dtpsService;
+            _parentRDMService = parentRDMService;
+            _childrenRDMService = childrenRDMService;
+        }
+
+        public IActionResult Create(int? dtpId)
+        {
+            var viewModel = new ParentFormViewModel
+            {
+                DTPId = dtpId.Value
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(ParentRDM parent, int dtpId)
+        {
+            parent.Ticket = await _dtpsService.FindByIdAsync(dtpId);
+            parent.OpenDate = DateTime.Now;
+
+            await _parentRDMService.InsertAsync(parent);
+
+            return RedirectToAction("Index", "DTPs");
+        }
+
+        public async Task<IActionResult> Delete(int? dtpId, int? parentId)
+        {
+            var parent = await _parentRDMService.FindByIdAsync(parentId.Value);
+            parent.Ticket = await _dtpsService.FindByIdAsync(dtpId.Value);
+
+            return View(parent);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _parentRDMService.RemoveAsync(id);
+
+            return RedirectToAction("Index", "DTPs");
+        }
+    }
+}
